d96f4b0791d6031acdb745a8548754473dc942a9
Roman-Nechaev <nrchaevroman@gmail.com>
2023-04-13 19:04:24 +0300

    fix import PageNotFound lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,13 @@ import { lazy } from 'react';
 
 import { Route, Routes } from 'react-router-dom';
 
-import PageNotFound from './error/PageNotFound';
-
 const Home = lazy(() => import('../pages/home/Home'));
 const Movies = lazy(() => import('../pages/movies/Movies'));
 const MovieDetails = lazy(() => import('../pages/movies/MovieDetails'));
 const Cast = lazy(() => import('./main/Cast'));
 const Reviews = lazy(() => import('./main/Reviews'));
 const Layout = lazy(() => import('./layout/Layout'));
+const PageNotFound = lazy(() => import('./error/PageNotFound'));
 
 export const App = () => {
   return (
